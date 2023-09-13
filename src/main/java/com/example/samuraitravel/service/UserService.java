d562ec6ef66906582fac47bb7dcbd2e2059a85b8
takai <takai@MyComputer>
2023-09-13 22:30:15 +0900

    メール認証機能を作成まで

--- src/main/java/com/example/samuraitravel/service/UserService.java
+++ src/main/java/com/example/samuraitravel/service/UserService.java
@@ -35,7 +35,7 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		user.setEnabled(true);
+		user.setEnabled(false);
 
 		return userRepository.save(user);
 	}
@@ -49,5 +49,12 @@ public class UserService {
 	// パスワードとパスワード（確認用）の入力値が一致するかどうかをチェックする
 	public boolean isSamePassword(String password, String passwordConfirmation) {
 		return password.equals(passwordConfirmation);
+	}
+
+	// ユーザーを有効にする
+	@Transactional
+	public void enableUser(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
 	}
 }
