be7d25e11a3c9047b55ae81c00c74679852ce677
takai <takai@MyComputer>
2023-09-19 22:42:03 +0900

    並べ替え機能を作成まで

--- src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -6,6 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.samuraitravel.entity.House;
 
-public interface HouseRepository extends JpaRepository<House, Integer>{
+public interface HouseRepository extends JpaRepository<House, Integer> {
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByCreatedAtDesc(String area, Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByPriceAsc(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable);
+
+	public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+	public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
 }
