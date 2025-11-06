1fc39583ef51efbc8c6bf14a7e3b0a33d1027d5c
takai <takainobutaka@gmail.com>
2025-11-06 22:53:55 +0900

    commit

--- src/main/java/com/example/samuraitravel/controller/HouseController.java
+++ src/main/java/com/example/samuraitravel/controller/HouseController.java
@@ -1,9 +1,12 @@
 package com.example.samuraitravel.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +14,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.samuraitravel.entity.Favorite;
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.entity.Review;
+import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.form.ReservationInputForm;
+import com.example.samuraitravel.repository.FavoriteRepository;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.repository.ReviewRepository;
+import com.example.samuraitravel.security.UserDetailsImpl;
+import com.example.samuraitravel.service.FavoriteService;
+import com.example.samuraitravel.service.ReviewService;
 
 @Controller
 @RequestMapping("/houses")
 public class HouseController {
 	private final HouseRepository houseRepository;
+	private final ReviewRepository reviewRepository;
+	private final ReviewService reviewService;
+	private final FavoriteRepository favoriteRepository;
+	private final FavoriteService favoriteService;
 
-	public HouseController(HouseRepository houseRepository) {
+	public HouseController(HouseRepository houseRepository, ReviewRepository reviewRepository,
+			ReviewService reviewService, FavoriteRepository favoriteRepository, FavoriteService favoriteService) {
 		this.houseRepository = houseRepository;
+		this.reviewRepository = reviewRepository;
+		this.reviewService = reviewService;
+		this.favoriteRepository = favoriteRepository;
+		this.favoriteService = favoriteService;
 	}
 
 	@GetMapping
@@ -71,11 +91,32 @@ public class HouseController {
 	}
 
 	@GetMapping("/{id}")
-	public String show(@PathVariable(name = "id") Integer id, Model model) {
+	public String show(@PathVariable(name = "id") Integer id, Model model,
+			@AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
 		House house = houseRepository.getReferenceById(id);
+		Favorite favorite = null;
+		boolean hasUserAlreadyReviewed = false;
+		boolean isFavorite = false;
+
+		if (userDetailsImpl != null) {
+			User user = userDetailsImpl.getUser();
+			hasUserAlreadyReviewed = reviewService.hasUserAlreadyReviewed(house, user);
+			isFavorite = favoriteService.isFavorite(house, user);
+			if (isFavorite) {
+				favorite = favoriteRepository.findByHouseAndUser(house, user);
+			}
+		}
+
+		List<Review> newReviews = reviewRepository.findTop6ByHouseOrderByCreatedAtDesc(house);
+		long totalReviewCount = reviewRepository.countByHouse(house);
 
 		model.addAttribute("house", house);
 		model.addAttribute("reservationInputForm", new ReservationInputForm());
+		model.addAttribute("favorite", favorite);
+		model.addAttribute("hasUserAlreadyReviewed", hasUserAlreadyReviewed);
+		model.addAttribute("newReviews", newReviews);
+		model.addAttribute("totalReviewCount", totalReviewCount);
+		model.addAttribute("isFavorite", isFavorite);
 
 		return "houses/show";
 	}
